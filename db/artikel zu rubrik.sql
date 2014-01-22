2dc776aacf3e5f007156f2c55e21306425cc8b7e
Schwoisser <milan298@gmx.de>
2014-01-22 17:41:57 +0100

    Delete artikel zu rubrik.sql

--- db/artikel zu rubrik.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Artikel_Rubrik
-(
-ArtikelId int NOT NULL,
-RubrikId int NOT NULL,
-
-FOREIGN KEY (ArtikelId) REFERENCES Artikel(Id),
-FOREIGN KEY (RubrikId) REFERENCES Rubrik(RubrikId)
-)
