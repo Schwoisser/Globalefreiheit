3ccbf4cb6c42653f6c3642e867a4547e780f2cc1
Schwoisser <milan298@gmx.de>
2014-02-01 17:59:20 +0100

    Update article.rb

--- models/article.rb
+++ models/article.rb
@@ -1,11 +1,24 @@
 class Article < ActiveRecord::Base
-  attr_accessible  :ID,:title,:author,:image,:text,:description,:tag,:link
+  attr_accessible  :id,:title,:author,:image, :preview_image,:text,:description,:tag,:link
   
   
   def page(page_nr)
+      article = last(2)
+      last_id = article.first.ID.to_i
+      article = where("ID < ? AND ID > ?", @last_id - (@pagenumber*7) ,@last_id - (@pagenumber*7+7) )
+  end
+  
+  def image
+    image = super
+    if image == nil
+      return id + ".jpg"
+    else
+      return image
+    end
   end
   
   def rubrik(rubrik_name)
+    
   end
   
 end
