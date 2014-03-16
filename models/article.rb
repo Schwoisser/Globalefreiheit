952472f320da65000eb7f43ea3ab9034db7efb0d
Schwoisser <milan298@gmx.de>
2014-03-16 18:44:37 +0100

    Update article.rb

--- models/article.rb
+++ models/article.rb
@@ -2,18 +2,6 @@ class Article < ActiveRecord::Base
   attr_accessible  :id,:title,:author,:image, :preview_image,:text,:description,:tag,:link
   self.primary_key = 'id'
   
-  def self.page (page_nr)
-      article = last(2)
-      last_id = article.first.id.to_i
-      from =last_id - (page_nr*7+7)
-      to =last_id - (page_nr*7)
-      puts from
-      puts to
-      article = find((from..to).to_a)
-      puts article
-      return article
-  end
-  
   def image
     image = super
     if image == nil
