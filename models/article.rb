7c5eae7429e58897e03cec1745e0205a6f6afe79
Milan Pogadl <milan.pogadl@xailabs.de>
2014-06-25 18:30:55 +0200

    refact article

--- models/article.rb
+++ models/article.rb
@@ -1,5 +1,13 @@
 class Article < ActiveRecord::Base
   self.primary_key = 'id'
+
+  def page(nr)
+    article = last
+    last_id = article.id.to_i
+    from =last_id - (nr*7+6)
+    to =last_id - (nr*7)
+    find((from..to).to_a).reverse!
+  end
   
   def image
     image = super
